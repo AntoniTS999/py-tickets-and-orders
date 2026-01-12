de806d8e7683ea6eee59d587f23478acac5c2f60
Anton Tsikhanovich <alluspeh@gmail.com>
2026-01-12 22:33:43 +0100

    Solution

--- services/movie.py
+++ services/movie.py
@@ -12,14 +12,14 @@ def get_movies(
 
     if title is not None:
         queryset = Movie.objects.all().filter(title__icontains=title)
-
+    else:
         if genres_ids:
             queryset = queryset.filter(genres__id__in=genres_ids)
 
         if actors_ids:
             queryset = queryset.filter(actors__id__in=actors_ids)
 
-        return queryset
+    return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
