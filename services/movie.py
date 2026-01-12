ab7a32aa4574b4545282b9ea8a457db5549395e5
Anton Tsikhanovich <alluspeh@gmail.com>
2026-01-12 22:54:47 +0100

    Solution

--- services/movie.py
+++ services/movie.py
@@ -10,8 +10,10 @@ def get_movies(
     title: str = None,
 ) -> QuerySet:
 
+    queryset = Movie.objects.all()
+
     if title is not None:
-        queryset = Movie.objects.all().filter(title__icontains=title)
+        queryset = queryset.filter(title__icontains=title)
     else:
         if genres_ids:
             queryset = queryset.filter(genres__id__in=genres_ids)
@@ -26,20 +28,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
     genres_ids: list = None,
     actors_ids: list = None,
 ) -> Movie:
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-        return movie
+    return movie
